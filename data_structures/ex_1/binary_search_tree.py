c1bed3b0b6b8e5abf2873732940e49ae42daa716
MarynaLongnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2018-07-20 14:04:04 -0400

    Update binary_search_tree.py

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -1,46 +1,79 @@
+class Node:
+    def __init__(self, value=None, L=None, R=None):
+        self.value = value
+        self.L = L
+        self.R = R
+
+
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, root=None, L=None, R=None):
+        self.root = Node(root)
+        self.L = L
+        self.R = R
 
-  def depth_first_for_each(self, cb):
-    pass    
+    def depth_first_for_each(self):
+        nodes = []
+        stack = [self.root]
+        while stack:
+            cur_node = stack.pop(0)
+            if cur_node not in nodes:
+                nodes.append(cur_node)
+                if cur_node.L:
+                    if cur_node.L not in nodes:
+                        stack.append(cur_node.L)
+                if cur_node.R:
+                    if cur_node.R not in nodes:
+                        stack.append(cur_node.R)
+        return [n.value for n in nodes]
 
-  def breadth_first_for_each(self, cb):
-    pass
+    def breadth_first_for_each(self):
+        nodes = []
+        stack = [self.root]
+        while stack:
+            cur_node = stack[0]
+            if cur_node is None: break
+            stack = stack[1:]
+            nodes.append(cur_node)
+            if cur_node.L: stack.append(cur_node.L)
+            if cur_node.R: stack.append(cur_node.R)
+        return [n.value for n in nodes]
 
-  def insert(self, value):
-    new_tree = BinarySearchTree(value)
-    if (value < self.value):
-      if not self.left:
-        self.left = new_tree
-      else:
-        self.left.insert(value)
-    elif value >= self.value:
-      if not self.right:
-        self.right = new_tree
-      else:
-        self.right.insert(value)
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(self.root, value)
 
-  def contains(self, target):
-    if self.value == target:
-      return True
-    if self.left:
-      if self.left.contains(target):
-        return True
-    if self.right:
-      if self.right.contains(target):
-        return True
-    return False
+    def _insert(self, node, value):
+        if node.value > value:
+            if node.L is None:
+                node.L = Node(value)
 
-  def get_max(self):
-    if not self:
-      return None
-    max_value = self.value
-    current = self
-    while current:
-      if current.value > max_value:
-        max_value = current.value
-      current = current.right
-    return max_value
+            else:
+                self._insert(node.L, value)
+        else:
+            if node.R is None:
+                node.R = Node(value)
+            else:
+                self._insert(node.R, value)
+
+    def contains(self, target):
+        if self.root is None:
+            return None
+        node = self.root
+        while node is not None:
+            if node.value == target:
+                return True
+            if target >= node.value:
+                node = node.R
+            else:
+                node = node.L
+        return False
+
+    def get_max(self):
+        if self.root is None:
+            return None
+        node = self.root
+        while node.R is not None:
+            node = node.R
+        return node.value
